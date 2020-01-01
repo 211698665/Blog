0c7a69a183067a7607af6989d053d91812a3348d
211698665 <47618129+211698665@users.noreply.github.com>
2020-01-01 21:05:59 +0800

    就删除了一个空行

--- src/main/java/cn/henu/HenuApplication.java
+++ src/main/java/cn/henu/HenuApplication.java
@@ -10,7 +10,6 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @EnableTransactionManagement
 public class HenuApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(HenuApplication.class, args);
     }
